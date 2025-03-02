11b57bdc5db41f27c64075bcec74601197f3c785
ndpower <alexeyvisnevskiy@gmail.com>
2025-03-02 22:41:17 +0200

    update class document, add test to console

--- ConsoleApp/Document.cs
+++ ConsoleApp/Document.cs
@@ -1,57 +1,69 @@
-﻿using QRCoder; 
+﻿using QRCoder;
+using System;
+using System.ComponentModel;
 using System.Drawing;
 
 namespace ConsoleApp
 {
-    abstract class Document
+    public abstract class Document
     {
-        public Action<string> _output = (str) => Console.WriteLine(str);
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Photo { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Gender { get; set; }
 
-        public string LastName;
-        public string FirstName;
-        public string Photo;
-        public int Year;
 
         protected Document(
             string LastName,
             string FirstName,
             string Photo,
-            int Year
+            string DateOfBirth,
+            string Gender
             )
         {
             this.LastName = LastName;
             this.FirstName = FirstName;
             this.Photo = Photo;
-            this.Year = Year;
+            this.DateOfBirth = DateOfBirth;
+            this.Gender = Gender;
         }
 
-        public string FullName()
-        {
-            return $"{LastName} {FirstName}";
-        } 
+        public string FullName() => $"{LastName} {FirstName}";
 
-        public void SetOutput(Action<string> output)
-        {
-            _output = output;
-        }
-
+        public abstract string ShowInfo();
         public abstract Bitmap generateQRCode();
     }
 
-    class Passport : Document
+    public class Passport : Document
     {
         private bool isHide;
-        public string passportId;
+        private string passportId;
+
+
+        public string DateOfExpiry { get; set; }
+        public string DateOfIssue { get; set; }
+        public string Nationality { get; set; }
+
 
         public Passport(
             string LastName, 
             string FirstName, 
             string Photo, 
-            int Year, 
+            string DateOfBirth,
+            string Gender,
+
+            string Nationality,
+            string DateOfExpiry,
+            string DateOfIssue,
+
 
             string passportId
-            ) : base(LastName, FirstName, Photo, Year)
+            ) : base(LastName, FirstName, Photo, DateOfBirth, Gender)
         {
+            this.Nationality= Nationality;
+            this.DateOfExpiry= DateOfExpiry;
+            this.DateOfIssue= DateOfIssue;
             this.passportId = passportId;
         }
 
@@ -61,21 +73,25 @@ namespace ConsoleApp
             isHide = !isHide;
         }
 
-        public void ShowInfo()
+        public string CopyIdentificationCode() => $"Code: {passportId}";
+
+        public override string ShowInfo()
         {
-            if (!isHide)
+            if (isHide)
             {
-               _output("Information not available!");
-                return;
+                return "Information is hidden!";
             }
 
-            _output(
-                $"Name: {LastName} {FirstName}\n" + 
-                $"Photo: {Photo}\n" + 
-                $"Year: {Year}\n" + 
-                $"PassId: {passportId}"
-            );
+            return $"Name: {FullName()}\n" +
+                   $"Gender: {Gender}\n" +
+                   $"Photo: {Photo}\n" +
+                   $"Date of Birth: {DateOfBirth}\n" +
+                   $"Passport ID: {passportId}\n" +
+                   $"Nationality: {Nationality}\n" +
+                   $"Date of Issue: {DateOfIssue}\n" +
+                   $"Date of Expiry: {DateOfExpiry}";
         }
+
 
         public override Bitmap generateQRCode()
         {
@@ -87,25 +103,111 @@ namespace ConsoleApp
         }
     }
 
-    class DriveLicense : Document
+    public class Military: Document
     {
-        public string licenseId;
+        public string militaryId { get; set; }
+        public string DateOfIssue { get; set; }
+        public string Rank { get; set; }
+
+        public Military(
+            string LastName,
+            string FirstName,
+            string Photo,
+            string DateOfBirth,
+            string Gender,
+
+            string militaryId,
+            string DateOfIssue,
+            string Rank
+            ) : base(LastName, FirstName, Photo, DateOfBirth, Gender)
+        {
+            this.militaryId = militaryId;
+            this.DateOfIssue = DateOfIssue;
+            this.Rank = Rank;
+        }
+
+        public string CopyIdentificationCode() => $"Code: {militaryId}";
+
+
+        public override string ShowInfo()
+        {
+            return $"Name: {FullName()}\n" +
+                   $"Gender: {Gender}\n" +
+                   $"Photo: {Photo}\n" +
+                   $"Date of Birth: {DateOfBirth}\n" +
+                   $"Date of Issue: {DateOfIssue}\n" +
+                   $"Military ID: {militaryId}\n" +
+                   $"Rank: {Rank}";
+        }
+
+        public override Bitmap generateQRCode()
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode($"{FullName()}_{militaryId}_{DateTime.Now}", QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            return qrCodeImage;
+        }
+    }
+
+    public class Car
+    {
+        public string Mark { get; set; }
+        public string Number { get; set; }
+        public string VIN { get; set; }
+
+        public Car(string mark, string number, string vIN)
+        {
+            Mark = mark;
+            Number = number;
+            VIN = vIN;
+        }
+    }
+
+    public class DriveLicense : Document
+    {
+        public string licenseId { get; set; }
+        public string passportId { get; set; }
 
         public List<string> licenseCategory;
 
+        public List<Car> cars;
+
         public DriveLicense(
-            string LastName, 
-            string FirstName, 
-            string Photo, 
-            int Year, 
+            string LastName,
+            string FirstName,
+            string Photo,
+            string DateOfBirth,
+            string Gender,
 
-            string licenseId, 
-            List<string> licenseCategory
+            string licenseId,
+            List<string>? licenseCategory,
+            List<Car>? cars
 
-            ) : base(LastName, FirstName, Photo, Year)
+            ) : base(LastName, FirstName, Photo, DateOfBirth, Gender)
         {
             this.licenseId = licenseId;
             this.licenseCategory = licenseCategory;
+            this.cars = cars;
+        }
+
+        public string CopyIdentificationCode() => $"Code: {licenseId}";
+
+        public void addLicCar(string category) => licenseCategory.Add(category);
+        public void delLicCar(string category) => licenseCategory.RemoveAt(licenseCategory.IndexOf(category));
+
+        public void addCar(Car car) => cars.Add(car);
+        public void delCar(string VIN) => cars.RemoveAt(cars.FindIndex(c => c.VIN.Equals(VIN)));
+
+        public override string ShowInfo()
+        {
+            return $"Name: {FullName()}\n" +
+                   $"Gender: {Gender}\n" +
+                   $"Photo: {Photo}\n" +
+                   $"Date of Birth: {DateOfBirth}\n" +
+                   $"License ID: {licenseId}\n" +
+                   $"License Category: {string.Join(", ", licenseCategory)}\n" +
+                   $"Cars: {string.Join(", ", cars.Select(c => c.Mark))}";
         }
 
         public override Bitmap generateQRCode()
@@ -115,6 +217,42 @@ namespace ConsoleApp
             QRCode qrCode = new QRCode(qrCodeData);
             Bitmap qrCodeImage = qrCode.GetGraphic(20);
             return qrCodeImage;
+        }
+    }
+
+    public class DocumentContainer
+    {
+        public Action<string> _output = (str) => Console.WriteLine(str);
+
+        private List<Document> documents = new List<Document>();
+
+        public void swapDocument(int i1, int i2)
+        {
+            var temp = documents[i1];
+            documents[i1] = documents[i2];
+            documents[i1] = temp;
+        }
+
+        public void addDoc(Document doc) => documents.Add(doc);
+        public void delDoc(int index) => documents.RemoveAt(index);
+
+        public List<Document> getDocuments() => documents;
+
+        public List<Document> findDocFullName(string lastName, string firstName) =>
+            documents.FindAll(d => d.FirstName == firstName && d.LastName == lastName).ToList();
+
+
+        public void SetOutput(Action<string> output)
+        {
+            _output = output;
+        }
+
+        public void showAllInfo()
+        {
+            foreach(Document doc in documents)
+            {
+               _output.Invoke($"{doc.ShowInfo()}\n====================");
+            }
         }
     }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+using ConsoleApp;
+
+DocumentContainer container = new DocumentContainer();
+
+Passport passport = new Passport(
+    "Ivanov", "Ivan",
+    "photo1.jpg", "01.01.1980", 
+    "Male", "Ukrainian", 
+    "01.01.2030", "01.01.2020", 
+    "P12345678"
+);
+
+Military military = new Military(
+    "Petrov", "Petr",
+    "photo2.jpg", "15.03.1990", 
+    "Male", "M1234567", "01.01.2025", 
+    "Colonel"
+);
+
+Car car1 = new Car("Toyota", "AB1234", "VIN12345");
+Car car2 = new Car("BMW", "CD5678", "VIN67890");
+
+List<string> category = new List<string> { "A", "B", "C1" };
+List<Car> cars = new List<Car> { car1, car2 };
+
+DriveLicense driveLicense = new DriveLicense(
+    "Petrov", "Petr", 
+    "photo3.jpg", "15.03.1990", 
+    "Male", "DL123456",
+    category, cars
+);
+
+container.addDoc(passport);
+container.addDoc(military);
+container.addDoc(driveLicense);
+
+container.SetOutput(WriteLine);
+
+WriteLine("== ALL Document ==");
+container.showAllInfo();
